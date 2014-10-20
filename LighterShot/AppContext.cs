db4d2b4a2008995f602e96b08d89e7faf89350d1
Dmitrijs Sadovskis <sdo@inbox.lv>
2014-10-20 13:19:22 +0300

    ammend

---
+++ LighterShot/AppContext.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace LighterShot
+{
+    class AppContext : ApplicationContext
+    {
+        public static FormMain FormMain;
+
+        public AppContext()
+        {
+            FormMain = new FormMain();
+        }
+    }
+}
