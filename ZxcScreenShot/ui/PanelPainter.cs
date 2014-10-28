3f703c71e8f77a9b8242473b362da618cf9f96c0
Dmitrijs Sadovskis <sdo@inbox.lv>
2014-10-28 15:59:45 +0200

    PanelPainter now implements Control

--- ZxcScreenShot/ui/PanelPainter.cs
+++ ZxcScreenShot/ui/PanelPainter.cs
@@ -6,11 +6,10 @@ using System.Windows.Forms;
 
 namespace ZxcScreenShot.ui
 {
-    class PanelPainter
+    class PanelPainter : Control
     {
         public delegate void InvalidatePanel(Rectangle invalidateRectangle);
-
-        private Rectangle BOUNDS = new Rectangle(new Point(100, 100), new Size(300, 50));
+        
         private readonly InvalidatePanel _panelInvalidates;
 
         private readonly List<Button> _buttons = new List<Button>();
@@ -19,28 +18,9 @@ namespace ZxcScreenShot.ui
 
         public PanelPainter(Rectangle bounds, InvalidatePanel panelInvalidates)
         {
-            BOUNDS = bounds;
+            Bounds = bounds;
             _panelInvalidates = panelInvalidates;
-            BOUNDS.Y -= 40;
-        }
-
-        public Rectangle Bounds
-        {
-            get { return BOUNDS; }
-        }
-
-        public int Top {
-            get { return BOUNDS.Y; }
-            set { BOUNDS.Y = value; }
-        }
-
-        public int Left {
-            get { return BOUNDS.X; }
-            set { BOUNDS.X = value; }
-        }
-
-        public int Height {
-            get { return BOUNDS.Height; }
+            Top -= 40;
         }
 
         public void AddButton(Button btn, EventHandler clickHandler)
@@ -52,19 +32,19 @@ namespace ZxcScreenShot.ui
         public void MouseMove(MouseEventArgs e)
         {
             _mouseLocation = e.Location;
-            if (BOUNDS.Contains(_mouseLocation))
+            if (Bounds.Contains(_mouseLocation))
             {
-                _panelInvalidates.Invoke(BOUNDS);
+                _panelInvalidates.Invoke(Bounds);
             }
         }
 
         public void DrawThePanel(Graphics g)
         {
             var panelMouseLocation = _mouseLocation;
-            panelMouseLocation.X -= BOUNDS.Left;
-            panelMouseLocation.Y -= BOUNDS.Top;
+            panelMouseLocation.X -= Left;
+            panelMouseLocation.Y -= Top;
 
-            g.FillRectangle(Brushes.White, BOUNDS);
+            g.FillRectangle(Brushes.White, Bounds);
 
             foreach (var button in _buttons)
             {
@@ -74,20 +54,20 @@ namespace ZxcScreenShot.ui
                 var fillBrush = button.Bounds.Contains(panelMouseLocation) ? Brushes.Azure : Brushes.White;
                 var borderPen = button.Bounds.Contains(panelMouseLocation) ? Pens.LightBlue : Pens.Gray;
 
-                g.FillRectangle(fillBrush, BOUNDS.X + button.Left, BOUNDS.Y + button.Top, button.Bounds.Width, button.Bounds.Height);
-                g.DrawRectangle(borderPen, BOUNDS.X + button.Left + 1, BOUNDS.Y + button.Top + 1, button.Bounds.Width - 3, button.Bounds.Height - 3);
+                g.FillRectangle(fillBrush, Bounds.X + button.Left, Bounds.Y + button.Top, button.Bounds.Width, button.Bounds.Height);
+                g.DrawRectangle(borderPen, Bounds.X + button.Left + 1, Bounds.Y + button.Top + 1, button.Bounds.Width - 3, button.Bounds.Height - 3);
 
                 var deltaToCenterImageX = (button.Width - buttonImage.Width)/2;
                 var deltaToCenterImageY = (button.Height - buttonImage.Height) / 2;
-                g.DrawImage(buttonImage, BOUNDS.X + button.Left + deltaToCenterImageX, BOUNDS.Y + button.Top + deltaToCenterImageY); // TODO: center
+                g.DrawImage(buttonImage, Bounds.X + button.Left + deltaToCenterImageX, Bounds.Y + button.Top + deltaToCenterImageY); // TODO: center
             }
         }
 
         public void MouseUp(MouseEventArgs e)
         {
             var panelMouseLocation = _mouseLocation;
-            panelMouseLocation.X -= BOUNDS.Left;
-            panelMouseLocation.Y -= BOUNDS.Top;
+            panelMouseLocation.X -= Left;
+            panelMouseLocation.Y -= Top;
 
             foreach (var button in _buttons)
             {
